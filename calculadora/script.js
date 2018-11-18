c9a3193645b62781d56b7a3930f04135c85814fd
Fellipe Marques <43136748+FellipeMarques@users.noreply.github.com>
2018-11-18 17:15:33 -0200

    Delete script.js

--- calculadora/script.js
+++
@@ -1,7 +0,0 @@
-var cap = prompt("Digite o valor inicial: ");
-var i = prompt("Digite o valor da taxa de juros aplicada(EX: 0.74): ");
-var temp = prompt("Digite o tempo em mês total pra pagar: ")
-var mont = cap * Math.pow((1 + i/100), temp);
-var montfinal = parseFloat(mont.toFixed(2));
-alert("O valor final será de: " +montfinal);
-
